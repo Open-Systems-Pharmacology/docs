a515ab4d548c219d807b32cffb64a4a1634eba25
Michael Sevestre <michael@design2code.ca>
2021-06-24 15:55:08 -0400

    Fixes #102 remove scripts for pdf (#137)

--- running-t.js
+++
@@ -1,8 +0,0 @@
-exports.header = null
-
- exports.footer = {
-  contents: function(pageNum, numPages) {
-    return "<p style='text-align: center; font-size: 12px;'>" + pageNum + " of " + numPages + "</p>"
-  },
-  height: '1cm'
-}
